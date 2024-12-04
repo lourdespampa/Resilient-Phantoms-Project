b7ce110c53737ba291ee15e2aa7741e53316ed25
LauMar2002 <laurenfernandez963@gmail.com>
2024-12-04 08:59:36 -0800

    Changed await in test_service

--- test/test_services.py
+++ test/test_services.py
@@ -3,7 +3,7 @@ import service.itunes as itunes
 
 @pytest.mark.asyncio
 async def test_service():
-    artist = itunes.get_artist("The Beatles", 3)
+    artist = await itunes.get_artist("The Beatles", 3)
     desc = str(artist)
     assert len(desc) > 0
 
